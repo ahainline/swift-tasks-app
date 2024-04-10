caf8cce10ffd44a441575f3de584b6d877e84c89
Ashley <tindle-taco@Marks-MacBook-Pro.local>
2024-04-10 18:09:03 -0500

    set done state by clicking checkbox

--- Tasks/ViewModels/ToDoListItemViewViewModel.swift
+++ Tasks/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Ashley on 4/1/24.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 //ViewModel for single to do list item view (each row in items list)
@@ -13,7 +15,19 @@ class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
     
 }
